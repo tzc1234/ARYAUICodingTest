45acc2b39b21ee3f5fb26dd8f3b072b8451c8f53
tzc1234 <peopli9997@gmail.com>
2025-03-15 23:24:04 +0000

    Add default array of DisplayMessage

--- ARYAUICodingTest/Views/Display Models/DisplayMessage.swift
+++ ARYAUICodingTest/Views/Display Models/DisplayMessage.swift
@@ -14,3 +14,22 @@ struct DisplayMessage: Identifiable {
     let isMine: Bool
     let isRead: Bool
 }
+
+extension [DisplayMessage] {
+    static var `default`: Self {
+        [
+            DisplayMessage(
+                text: "Hey John, let's get together and discuss the job proposal. Does Monday Work?",
+                date: "11:48 AM",
+                isMine: false,
+                isRead: true
+            ),
+            DisplayMessage(
+                text: "That would be great. Yes, I will see you on Monday.",
+                date: "1:54 PM",
+                isMine: true,
+                isRead: true
+            ),
+        ]
+    }
+}
