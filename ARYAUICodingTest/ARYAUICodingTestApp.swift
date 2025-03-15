3525f7a67fc249db21f2d778a4eb14718097cac4
tzc1234 <peopli9997@gmail.com>
2025-03-15 09:44:17 +0000

    Initial Commit

---
+++ ARYAUICodingTest/ARYAUICodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARYAUICodingTestApp.swift
+//  ARYAUICodingTest
+//
+//  Created by Tsz-Lung on 15/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ARYAUICodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
